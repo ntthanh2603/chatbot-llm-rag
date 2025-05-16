816ced2030b6a6bc1d2071b6797df9b306ae670b
Truong <127490669+dangtruong-github@users.noreply.github.com>
2025-05-16 16:54:01 +0700

    edit main

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-from script.embedding_module import Embedding
-from script.chromadb_module import ChromaDBClient
-from script.rag_module import RAG
-from script.llm_service import LLMService
+from embedding_module import Embedding
+from pinecone_module import PineconeDBClient
+from rag_module import RAG
+from llm_service import LLMService
 from dotenv import load_dotenv
 import os
 
@@ -12,33 +12,39 @@ PATH_DATA = "./crawl/data_vnu_wikipedia.txt"
 MODEL_EMBEDDING = os.getenv("MODEL_EMBEDDING")
 CHUNK_SIZE = 256
 
+
 def main():
     load_dotenv()
 
-    # with open(PATH_DATA, "r", encoding="utf-8") as f:
-    #     texts = f.read()
+    with open(PATH_DATA, "r", encoding="utf-8") as f:
+        texts = f.read()
 
     # Chunk data in file .txt
-    # embedding_handler = Embedding(model_embedding=MODEL_EMBEDDING, chunk_size=CHUNK_SIZE)
-    # chunks = embedding_handler.chunk_text(texts)
+    embedding_handler = Embedding(
+        model_embedding=MODEL_EMBEDDING,
+        chunk_size=CHUNK_SIZE
+    )
+    chunks = embedding_handler.chunk_text(texts)
 
     # Insert data to ChromaDB
-    # db = ChromaDBClient(model_embedding=MODEL_EMBEDDING, chunk_size=CHUNK_SIZE)
-    # texts_with_embeddings = [db.insert_with_text(chunk) for chunk in chunks]
-
+    db = PineconeDBClient(
+        model_embedding=MODEL_EMBEDDING,
+        chunk_size=CHUNK_SIZE
+    )
+    texts_with_embeddings = [db.insert_with_text(chunk) for chunk in chunks]
 
     # Query to ChromaDB
-    # result = db.query("Đại học Quốc gia Hà Nội")
-    # print('result', result)
+    result = db.query("Đại học Quốc gia Hà Nội")
+    print('result', result)
 
     # Query in RAG
-    # rag = RAG(model_embedding=MODEL_EMBEDDING, chunk_size=CHUNK_SIZE)
-    # result = rag.rag_query("Đại học Quốc gia Hà Nội")
-    # print('result', result)
+    rag = RAG(model_embedding=MODEL_EMBEDDING, chunk_size=CHUNK_SIZE)
+    result = rag.rag_query("Đại học Quốc gia Hà Nội")
+    print('result', result)
 
     llm_service = LLMService(
         # You can specify a different model from the list if needed
-        # model_name='meta-llama/Llama-3.1-8B-Instruct',
+        model_name='meta-llama/Llama-3.1-8B-Instruct',
         use_rag=True,
         model_embedding=MODEL_EMBEDDING
     )
@@ -47,13 +53,13 @@ def main():
     print(f"\n--- Query 1: {prompt1} ---")
     output1 = llm_service.generate_text(prompt1)
     print("output1", output1)
-    
+
     # Example 2: Vietnamese query with RAG
     prompt2 = "Đại học Quốc Gia Hà Nội có những trường thpt nào?"
     print(f"\n--- Query 2: {prompt2} ---")
     output2 = llm_service.generate_text(prompt2)
     print("output2", output2)
-    
+
     # Example 3: Direct query without RAG
     prompt3 = "Ai là Đại tướng đầu tiên của Quân đội Nhân dân Việt Nam?"
     print(f"\n--- Query 3 (without RAG): {prompt3} ---")
